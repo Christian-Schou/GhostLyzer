0c606ac1d8b65cceb4ee3447cac904d38bcfa306
Christian Schou <christian.schou@hotmail.com>
2023-12-22 10:44:23 +0100

    Add more usings

--- src/core/GhostMetrics.Core.Domain/GlobalUsings.cs
+++ src/core/GhostMetrics.Core.Domain/GlobalUsings.cs
@@ -1,1 +1,4 @@
 global using GhostMetrics.Core.Domain.Common;
+global using GhostMetrics.Core.Domain.Exceptions;
+global using GhostMetrics.Core.Domain.ValueObjects;
+global using GhostMetrics.Core.Domain.Events;
