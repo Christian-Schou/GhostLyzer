9df76ae3ca4b337256664c7eb5b5f22450f22d58
Christian Schou <christian.schou@hotmail.com>
2023-12-22 10:44:10 +0100

    Add color + exception

---
+++ src/core/GhostMetrics.Core.Domain/Exceptions/UnsupportedColourException.cs
@@ -0,0 +1,9 @@
+namespace GhostMetrics.Core.Domain.Exceptions;
+
+public class UnsupportedColourException : Exception
+{
+    public UnsupportedColourException(string code)
+        : base($"Colour \"{code}\" is unsupported.")
+    {
+    }
+}
