25b3de677daa30f3cdfc804d87e21c9ab1ac9ca4
Christian Schou <christian.schou@hotmail.com>
2023-12-22 10:10:42 +0100

    Add Domain Constants

---
+++ src/core/GhostMetrics.Core.Domain/Constants/Policies.cs
@@ -0,0 +1,6 @@
+namespace GhostMetrics.Core.Domain.Constants;
+
+public abstract class Policies
+{
+    public const string CanPurge = nameof(CanPurge);
+}
---
+++ src/core/GhostMetrics.Core.Domain/Constants/Roles.cs
@@ -0,0 +1,6 @@
+namespace GhostMetrics.Core.Domain.Constants;
+
+public abstract class Roles
+{
+    public const string Administrator = nameof(Administrator);
+}
