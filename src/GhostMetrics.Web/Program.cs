bc2d219c0c420cf7018b73d1d8cf26fb57600928
Christian Schou <christian.schou@hotmail.com>
2023-12-23 02:03:40 +0100

    Add NSwag

--- src/GhostMetrics.Web/Program.cs
+++ src/GhostMetrics.Web/Program.cs
@@ -1,6 +1,5 @@
 using GhostMetrics.Infrastructure;
 using GhostMetrics.Infrastructure.Data;
-using GhostMetrics.Infrastructure.Identity;
 using GhostMetrics.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,49 +8,33 @@ builder.Services.AddApplicationServices();
 builder.Services.AddWebServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-app.MapIdentityApi<ApplicationUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     await app.InitializeDatabaseAsync();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+}else
+{
+    // The default HSTS value is 30 days.
+    // You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-var summaries = new[]
+app.UseSwaggerUi(settings =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    settings.Path = "/api";
+    settings.DocumentPath = "/api/specification.json";
+});
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.UseExceptionHandler(options => { });
+
+app.MapEndpoints();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+public partial class Program { }
--- src/GhostMetrics.Web/Services/Startup.cs
+++ src/GhostMetrics.Web/Services/Startup.cs
@@ -1,6 +1,8 @@
 using GhostMetrics.Core.Application.Common.Interfaces;
 using GhostMetrics.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 using ZymLabs.NSwag.FluentValidation;
 
 namespace GhostMetrics.Web.Services;
@@ -9,6 +11,8 @@ public static class Startup
 {
     public static IServiceCollection AddWebServices(this IServiceCollection services)
     {
+        services.AddDatabaseDeveloperPageExceptionFilter();
+        
         services.AddScoped<IUser, CurrentUser>();
 
         services.AddHttpContextAccessor();
@@ -29,6 +33,30 @@ public static class Startup
         // Customize the default API behaviour
         services.Configure<ApiBehaviorOptions>(options =>
             options.SuppressModelStateInvalidFilter = true);
+        
+        services.AddEndpointsApiExplorer();
+
+        services.AddOpenApiDocument((configure, sp) =>
+        {
+            configure.Title = "GhostMetrics API";
+            
+            // Add the fluent validations schema processor
+            var fluentValidationSchemaProcessor = 
+                sp.CreateScope().ServiceProvider.GetRequiredService<FluentValidationSchemaProcessor>();
+            
+            configure.SchemaSettings.SchemaProcessors.Add(fluentValidationSchemaProcessor);
+            
+            // Add JWT
+            configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+            {
+                Type = OpenApiSecuritySchemeType.ApiKey,
+                Name = "Authorization",
+                In = OpenApiSecurityApiKeyLocation.Header,
+                Description = "Type into the textbox: Bearer {your JWT token}."
+            });
+            
+            configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+        });
         
         return services;
     }
