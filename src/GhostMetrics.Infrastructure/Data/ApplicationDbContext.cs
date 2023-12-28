1e6d347608c958e4b486a626e4869e171cb1548c
Christian Schou <christian.schou@hotmail.com>
2023-12-28 20:48:04 +0100

    Cleanup

--- src/GhostMetrics.Infrastructure/Data/ApplicationDbContext.cs
+++ src/GhostMetrics.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
 using GhostMetrics.Core.Application.Common.Interfaces;
+using GhostMetrics.Core.Domain.Entities.Analytics;
 using GhostMetrics.Core.Domain.Entities.Ghost;
+using GhostMetrics.Core.Domain.Entities.SEO;
 using GhostMetrics.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -11,12 +13,37 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IApplica
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
 
+    #region Site
+
     public DbSet<SiteList> SiteLists => Set<SiteList>();
     public DbSet<Site> Sites => Set<Site>();
     public DbSet<IntegrationDetail> SiteIntegrationDetails => Set<IntegrationDetail>();
+
+    #endregion
+
+    #region Posts & Authors
+
     public DbSet<Author> Authors => Set<Author>();
     public DbSet<Post> Posts => Set<Post>();
     public DbSet<PostAuthor> PostAuthors => Set<PostAuthor>();
+
+    #endregion
+
+    #region Analytics
+
+    public DbSet<AuthorAnalytics> AuthorAnalytics => Set<AuthorAnalytics>();
+    public DbSet<PostAnalytics> PostAnalytics => Set<PostAnalytics>();
+    public DbSet<TagAnalytics> TagAnalytics => Set<TagAnalytics>();
+    public DbSet<BaseAnalytics> BaseAnalytics => Set<BaseAnalytics>();
+
+    #endregion
+
+    #region Seo
+
+    public DbSet<PostSeo> PostSeo => Set<PostSeo>();
+    public DbSet<TagSeo> TagSeo => Set<TagSeo>();
+
+    #endregion
     
     protected override void OnModelCreating(ModelBuilder builder)
     {
