7c61dadb475c7f127b7f270724c9cef50887628b
Christian Schou <christian.schou@hotmail.com>
2023-12-30 13:21:31 +0100

    Refactor: Import Ghost objects from API
    When a new ghost site is added now, the objects, such as authors, tags, posts, settings, etc... will now be imported into the GhostMetrics database.

--- src/core/GhostMetrics.Core.Application/Features/Ghost/Sites/EventHandlers/SiteCreatedEventHandler.cs
+++ src/core/GhostMetrics.Core.Application/Features/Ghost/Sites/EventHandlers/SiteCreatedEventHandler.cs
@@ -1,3 +1,4 @@
+using GhostMetrics.Core.Application.Services.GhostMetrics;
 using GhostMetrics.Core.Domain.Events;
 using Microsoft.Extensions.Logging;
 
@@ -6,16 +7,20 @@ namespace GhostMetrics.Core.Application.Features.Ghost.Sites.EventHandlers;
 public class SiteCreatedEventHandler : INotificationHandler<GhostSiteCreatedEvent>
 {
     private readonly ILogger<SiteCreatedEventHandler> _logger;
+    private readonly IGhostMetricsDataService _ghostMetrics;
 
-    public SiteCreatedEventHandler(ILogger<SiteCreatedEventHandler> logger)
+    public SiteCreatedEventHandler(ILogger<SiteCreatedEventHandler> logger, IGhostMetricsDataService ghostMetrics)
     {
         _logger = logger;
+        _ghostMetrics = ghostMetrics;
     }
     
-    public Task Handle(GhostSiteCreatedEvent notification, CancellationToken cancellationToken)
+    public async Task Handle(GhostSiteCreatedEvent notification, CancellationToken cancellationToken)
     {
         _logger.LogInformation("GhostMetrics Domain Event: {DomainEvent}", notification.GetType().Name);
-        // TODO: Add some awesome stuff here!
-        return Task.CompletedTask;
+
+        // Import Ghost objects from Ghost Site API
+        await _ghostMetrics.ImportAndUpdateAuthorsForSiteAsync(notification.Site.Id, cancellationToken);
+        await _ghostMetrics.ImportAndUpdatePostsForSiteAsync(notification.Site.Id, cancellationToken);
     }
 }
