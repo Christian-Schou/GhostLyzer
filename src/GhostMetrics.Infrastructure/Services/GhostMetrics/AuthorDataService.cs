97a9e8c06c3a71f7ecaa2f0394ce250fcded6e35
Christian Schou <christian.schou@hotmail.com>
2023-12-30 11:07:22 +0100

    Refactor: Author add/update
    Extracted add/update implementation into two separate methods for better reusability across other methods. This is useful when adding a post from Ghost and the author has to be added/updated but the posts method doesn't want to care about that task.

--- src/GhostMetrics.Infrastructure/Services/GhostMetrics/AuthorDataService.cs
+++ src/GhostMetrics.Infrastructure/Services/GhostMetrics/AuthorDataService.cs
@@ -1,5 +1,6 @@
 using GhostMetrics.Core.Domain.Entities.Ghost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace GhostMetrics.Infrastructure.Services.GhostMetrics;
 
@@ -7,55 +8,75 @@ public partial class GhostMetricsDataService
 {
     public async Task ImportAndUpdateAuthorsForSiteAsync(Guid siteId, CancellationToken cancellationToken)
     {
-        // Retrieve site details
-        var site = await _context.Sites
-            .AsNoTracking()
-            .Include(x => x.IntegrationDetails)
-            .FirstAsync(x => x.Id == siteId, cancellationToken);
-
-        // Make sure we got a usable response from the database
-        if (string.IsNullOrEmpty(site.IntegrationDetails.ApiUrl) || string.IsNullOrEmpty(site.IntegrationDetails.ContentApiKey))
+        try
         {
-            return;
+            // Retrieve site details
+            var site = await _context.Sites
+                .AsNoTracking()
+                .Include(x => x.IntegrationDetails)
+                .FirstAsync(x => x.Id == siteId, cancellationToken);
+
+            // Make sure we have the required integration details
+            Guard.Against.NullOrEmpty(site.IntegrationDetails.ApiUrl);
+            Guard.Against.NullOrEmpty(site.IntegrationDetails.ContentApiKey);
+            
+            // Get all authors from the Ghost Site
+            var authors = _ghostApi
+                .GetAllAuthors(site.IntegrationDetails.ApiUrl, site.IntegrationDetails.ContentApiKey);
+
+            foreach (var author in authors)
+            {
+                await AddOrUpdateSingleGhostAuthorInDatabaseAsync(author, cancellationToken);
+            }
         }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "An unhandled error occured while trying to add or update all authors from Ghost site to GhostMetrics database");
+            throw;
+        }
+    }
 
-        // Get all authors from the Ghost Site
-        var authors = _ghostApi
-            .GetAllAuthors(site.IntegrationDetails.ApiUrl, site.IntegrationDetails.ContentApiKey);
-
-        foreach (var author in authors)
+    public async Task AddOrUpdateSingleGhostAuthorInDatabaseAsync(
+        GhostSharp.Entities.Author ghostAuthor, CancellationToken cancellationToken = default)
+    {
+        try
         {
             // Retrieve the existing author from the database
-            var existingAuthor = _context.Authors.FirstOrDefault(a => a.GhostAuthorId == author.Id);
+            var existingAuthor = _context.Authors.FirstOrDefault(a => a.GhostAuthorId == ghostAuthor.Id);
 
             if (existingAuthor != null)
             {
                 // Update the existing author if it already exists
-                existingAuthor.Name = author.Name;
-                existingAuthor.Slug = author.Slug;
-                existingAuthor.ProfileImage = author.ProfileImage;
-                existingAuthor.Bio = author.Bio;
-                existingAuthor.Url = author.Url;
+                existingAuthor.Name = ghostAuthor.Name;
+                existingAuthor.Slug = ghostAuthor.Slug;
+                existingAuthor.ProfileImage = ghostAuthor.ProfileImage;
+                existingAuthor.Bio = ghostAuthor.Bio;
+                existingAuthor.Url = ghostAuthor.Url;
 
                 _context.Authors.Update(existingAuthor);
                 await _context.SaveChangesAsync(cancellationToken);
             }
             else
             {
-                // 3. Create a new author if it doesn't exist
+                // Create a new author if it doesn't exist
                 var newAuthor = new Author
                 {
-                    GhostAuthorId = author.Id,
-                    Name = author.Name,
-                    Slug = author.Slug,
-                    ProfileImage = author.ProfileImage,
-                    Bio = author.Bio,
-                    Url = author.Url
+                    GhostAuthorId = ghostAuthor.Id,
+                    Name = ghostAuthor.Name,
+                    Slug = ghostAuthor.Slug,
+                    ProfileImage = ghostAuthor.ProfileImage,
+                    Bio = ghostAuthor.Bio,
+                    Url = ghostAuthor.Url
                 };
 
                 _context.Authors.Add(newAuthor);
                 await _context.SaveChangesAsync(cancellationToken);
             }
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "GhostMetrics: An unhandled error occured while trying to add or update Ghost author in GhostMetrics database");
+            throw;
         }
     }
 }
