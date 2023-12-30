f72426e5e89961c043c0499d01ffc19126fbe70a
Christian Schou <christian.schou@hotmail.com>
2023-12-30 13:18:01 +0100

    Refactor: Remove db context
    Removed the application db context, as all database logic has been implemented in the repositories and should be in the future for a clean and maintainable application.

--- src/GhostMetrics.Infrastructure/Services/GhostMetrics/GhostMetricsDataService.cs
+++ src/GhostMetrics.Infrastructure/Services/GhostMetrics/GhostMetricsDataService.cs
@@ -1,5 +1,4 @@
 using GhostMetrics.Core.Application.Common.Data;
-using GhostMetrics.Core.Application.Common.Interfaces;
 using GhostMetrics.Core.Application.Services.Ghost;
 using GhostMetrics.Core.Application.Services.GhostMetrics;
 
@@ -7,18 +6,14 @@ namespace GhostMetrics.Infrastructure.Services.GhostMetrics;
 
 public partial class GhostMetricsDataService : IGhostMetricsDataService
 {
-    private readonly IApplicationDbContext _context;
     private readonly ILogger<GhostMetricsDataService> _logger;
     private readonly IGhostApiService _ghostApi;
     private readonly IUnitOfWork _unitOfWork;
 
-    public GhostMetricsDataService(
-        IApplicationDbContext context,
-        ILogger<GhostMetricsDataService> logger,
+    public GhostMetricsDataService(ILogger<GhostMetricsDataService> logger,
         IGhostApiService ghostApi,
         IUnitOfWork unitOfWork)
     {
-        _context = context;
         _logger = logger;
         _ghostApi = ghostApi;
         _unitOfWork = unitOfWork;
